075be3cabf8af66fb800566c561507dcf8d63316
阿部圭一郎 <abekeiichirou@abekeiichirounoiMac.local>
2018-08-05 10:10:23 +0900

    初めてのGithub

---
+++ taitanic.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 28 22:01:08 2018
+
+@author: abekeiichirou
+"""
+
+import pandas as pd
+import numpy as np
+
+train = pd.read_csv("/Users/abekeiichirou/Desktop/all/train.csv")
+test = pd.read_csv("/Users/abekeiichirou/Desktop/all/test.csv")
+
+
+print(train.head())
+
+def kesson_table(df):
+
+    null_val = df.isnull().sum()
+    percent = 100 * df.isnull().sum()/len(df)
+    kesson_table = pd.concat([null_val, percent], axis=1)
+    kesson_table_ren_columns = kesson_table.rename(columns = {0 : '欠損数', 1: '%'})
+    return kesson_table_ren_columns
+
+print(kesson_table(train))
+
+train["Age"] = train["Age"].fillna(train["Age"].median())
+train["Embarked"] = train["Embarked"].fillna("S")
+
+train["Sex"][train["Sex"] == "male"] = 0
+train["Sex"][train["Sex"] == "female"] = 0
+train["Embarked"][train["Embarked"] == "S" ] = 0
+train["Embarked"][train["Embarked"] == "C" ] = 1
+train["Embarked"][train["Embarked"] == "Q"] = 2
+
+train.head(10)
+
+test["Age"] = test["Age"].fillna(test["Age"].median())
+test["Sex"][test["Sex"] == "male"] = 0
+test["Sex"][test["Sex"] == "female"] = 1
+test["Embarked"][test["Embarked"] == "S"] = 0
+test["Embarked"][test["Embarked"] == "C"] = 1
+test["Embarked"][test["Embarked"] == "Q"] = 2
+test.Fare[152] = test.Fare.median()
+ 
+test.head(10)
